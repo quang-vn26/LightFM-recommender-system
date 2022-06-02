11edd92fa6d1a99e9793ec7c8e27a0cb3534add0
quang_ngoc <nnquang.code@gmail.com>
2022-06-03 00:32:48 +0700

    add csv and pkl file

--- module/test_model.py
+++ module/test_model.py
@@ -22,8 +22,8 @@ import lightfm_data
 import train_model
 import recommender
 import os
-
-# rs_loaded = pickle.load(open("rs.h5","rb"))
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 ############################################
@@ -86,20 +86,67 @@ light_fm_data_prep = lightfm_data.LightFMDataPrep()
 train_lightfm = train_model.TrainLightFM()
 
 # process raw data
-df_questions_p, df_professionals_p, df_merge_p = \
-cv_data_prep.prepare(
-    df_professionals,df_students,
-    df_questions,df_answers,
-    df_tags,df_tag_questions,df_tag_users,
-    df_question_scores)
-# df_questions_p.to_csv("demo.csv")
+def pro_data():
+    df_questions_p, df_professionals_p, df_merge_p = \
+    cv_data_prep.prepare(
+        df_professionals,df_students,
+        df_questions,df_answers,
+        df_tags,df_tag_questions,df_tag_users,
+        df_question_scores)
+    if os.path.exists("df_questions_p.csv"):
+        os.remove("df_questions_p.csv")
+    else:
+        display_csv.to_csv("df_questions_p.csv") 
+
+    if os.path.exists("df_professionals_p.csv"):
+        os.remove("df_professionals_p.csv")
+    else:
+        display_csv.to_csv("df_professionals_p.csv") 
+
+    if os.path.exists("df_merge_p.csv"):
+        os.remove("df_merge_p.csv")
+    else:
+        display_csv.to_csv("df_merge_p.csv")   
+
+df_questions_p = pd.read_csv('df_questions_p.csv')
+df_professionals_p = pd.read_csv('df_professionals_p.csv')
+df_merge_p = pd.read_csv('df_merge_p.csv')
 
 # prepare data for lightfm 
-interactions, weights, \
-questions_features, professional_features = \
-light_fm_data_prep.fit(
-    df_questions_p, df_professionals_p, df_merge_p)
+def pre_data():
+    interactions, weights, \
+    questions_features, professional_features = \
+    light_fm_data_prep.fit(
+        df_questions_p, df_professionals_p, df_merge_p)
 
+    if os.path.exists("interactions.pkl"):
+        os.remove("interactions.pkl")
+    else:
+        with open('interactions.pkl','wb') as file:
+            pickle.dump(interactions,file)
+
+    if os.path.exists("weights.pkl"):
+        os.remove("weights.pkl")
+    else:
+        with open('weights.pkl','wb') as file:
+            pickle.dump(weights,file)
+
+    if os.path.exists("questions_features.pkl"):
+        os.remove("questions_features.pkl")
+    else:
+        with open('questions_features.pkl','wb') as file:
+            pickle.dump(questions_features,file)
+
+    if os.path.exists("professional_features.pkl"):
+        os.remove("professional_features.pkl")
+    else:
+        with open('professional_features.pkl','wb') as file:
+            pickle.dump(professional_features,file)
+
+# pre_data()
+
+questions_features = pickle.load(open("questions_features.pkl","rb"))
+professional_features = pickle.load(open("professional_features.pkl","rb"))
 
 # finally build and trian our model
 # model = train_lightfm.fit(interactions,
@@ -124,6 +171,10 @@ if os.path.exists("display_csv.csv"):
     os.remove("display_csv.csv")
 else:
     display_csv.to_csv("display_csv.csv")    
+
+def recommend_by_pro_id_date_rage(professional_id,start_date,end_date,num_prediction):
+    rc_by_day = lightfm_recommendations.recommend_by_pro_id_frequency_date_range(professional_id,start_date,end_date,num_prediction)
+    
 # create file
 # create_file_csv(display_csv)
 # display(display_csv)
